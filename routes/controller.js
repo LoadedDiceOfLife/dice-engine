116f699290fc65e6f32bcf36661663dbdaab9d8e
amelialaundy <amelialaundy@gmail.com>
2015-07-04 16:35:26 +1200

    add controller

--- routes/controller.js
+++ routes/controller.js
@@ -1,7 +1,26 @@
-var Controller = function () {};
-var mongo
-Controller.prototype.getProfile = function () {
+'use strict';
 
+var Controller = function () {};
+var MongoClient = require('mongodb').MongoClient;
+var url = 'http://strato.oberkirch.org:27017';
+
+
+
+Controller.prototype.getProfile = function (locations) {
+  MongoClient.connect(url, function (err, db) {
+
+
+    var collection = db.collection('documents');
+    // Find some documents 
+    collection.find({}).toArray(function (err, docs) {
+      console.dir(docs);
+      callback(docs);
+    });
+
+
+
+    db.close();
+  });
 };
 
 module.exports.controller = new Controller();
